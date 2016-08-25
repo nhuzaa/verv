d85c5b17656df7cce63035ca0fbd9e9ac75a316d
Rohit Sthapit <nhuzaa@gmail.com>
2016-08-26 02:53:29 +0545

    dd

--- walk.py
+++ walk.py
@@ -9,21 +9,32 @@ lHip = 27
 lKnee = 17
 lBase = 4
 
+# Right leg
+rHipRot = 6
+rHip = 13
+rKnee = 19
+rBase = 26
+
 lHipServo = servo(lHip, "lHip")
 lKneeServo = servo(lKnee, "lKnee")
 lBaseServo = servo(lBase, "lBase")
 
+rBaseServo = servo(rBase, "rBase")
+
 lHipServo.set(90)
 lKneeServo.set(90)
 lBaseServo.set(90)
+
+rBaseServo.set(90)
+
 while True:
-    lHipServo.slowset(0)
-    lKneeServo.slowset(180)
-    lBaseServo.slowset(20)
-
+    # lHipServo.slowset(0)
+    # lKneeServo.slowset(180)
+    lBaseServo.slowset(0)
+    rBaseServo.slowset(0)
     time.sleep(1)
-
-    lHipServo.slowset(180)
-    lKneeServo.slowset(0)
-    lBaseServo.slowset(80)
+    # lHipServo.slowset(180)
+    # lKneeServo.slowset(0)
+    lBaseServo.slowset(90)
+    rBaseServo.slowset(90)
     time.sleep(1)
