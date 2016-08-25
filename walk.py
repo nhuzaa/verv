bacc6395692d401df1c705073e69c05cd4d9e6fb
Rohit Sthapit <nhuzaa@gmail.com>
2016-08-26 02:23:24 +0545

    sdf

--- walk.py
+++ walk.py
@@ -11,10 +11,17 @@ lBase = 22
 
 lHipServo = servo(lHip, "lHip")
 lKneeServo = servo(lKnee, "lKnee")
+lHipRotServo = servo(lHipRot, "lHipRot")
+
+lHipServo.set(90)
+lKneeServo.set(90)
+lHipRotServo.set(90)
 while True:
     lHipServo.slowset(0)
     lKneeServo.slowset(180)
+    lHipRotServo.slowset(20)
     time.sleep(1)
     lHipServo.slowset(180)
     lKneeServo.slowset(0)
+    lHipRotServo.slowset(80)
     time.sleep(1)
