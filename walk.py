e02397d5dc34dc913b483937ff74b1a8783b305b
Rohit Sthapit <nhuzaa@gmail.com>
2016-08-26 04:24:23 +0545

    sdf

--- walk.py
+++ walk.py
@@ -30,52 +30,60 @@ lHipRotServo.set(70)
 rHipRotServo.set(70)
 while True:
 
-    # Preparation of Right leg up
-    # lean left
-    rBaseServo.set(160)
-    time.sleep(1)
-    lBaseServo.set(130)
-    time.sleep(1)
-
-    # Right UP
-    rKneeServo.set(180)
-    time.sleep(1)
-    rHipServo.set(0)
-    time.sleep(1)
-
     # center
-    rBaseServo.set(90)
+    x = raw_input("Right Base Servo")
+    rBaseServo.set(x)
     time.sleep(1)
+    x = raw_input("left Base Servo")
     lBaseServo.set(60)
     time.sleep(1)
-
-    # Right Down
-    rKneeServo.set(90)
-    time.sleep(1)
-    rHipServo.set(90)
-    time.sleep(1)
-
-    # Prepartion for Left Leg up
-    # Lean Right
-    rBaseServo.set(20)
-    time.sleep(1)
-    lBaseServo.set(0)
-    time.sleep(1)
-
-    # left UP
-    lKneeServo.set(0)
-    time.sleep(1)
-    lHipServo.set(180)
-    time.sleep(1)
-
-    # center
-    rBaseServo.set(90)
-    time.sleep(1)
-    lBaseServo.set(60)
-    time.sleep(1)
-
-    # Left Down
-    lKneeServo.set(90)
-    time.sleep(1)
-    lHipServo.set(90)
-    time.sleep(1)
+    #
+    # # Preparation of Right leg up
+    # # lean left
+    # rBaseServo.set(160)
+    # time.sleep(1)
+    # lBaseServo.set(130)
+    # time.sleep(1)
+    #
+    # # Right UP
+    # rKneeServo.set(180)
+    # time.sleep(1)
+    # rHipServo.set(0)
+    # time.sleep(1)
+    #
+    # # center
+    # rBaseServo.set(90)
+    # time.sleep(1)
+    # lBaseServo.set(60)
+    # time.sleep(1)
+    #
+    # # Right Down
+    # rKneeServo.set(90)
+    # time.sleep(1)
+    # rHipServo.set(90)
+    # time.sleep(1)
+    #
+    # # Prepartion for Left Leg up
+    # # Lean Right
+    # rBaseServo.set(20)
+    # time.sleep(1)
+    # lBaseServo.set(0)
+    # time.sleep(1)
+    #
+    # # left UP
+    # lKneeServo.set(0)
+    # time.sleep(1)
+    # lHipServo.set(180)
+    # time.sleep(1)
+    #
+    # # center
+    # rBaseServo.set(90)
+    # time.sleep(1)
+    # lBaseServo.set(60)
+    # time.sleep(1)
+    #
+    # # Left Down
+    # lKneeServo.set(90)
+    # time.sleep(1)
+    # lHipServo.set(90)
+    # time.sleep(1)
